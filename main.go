2ddb2383a9221a4b25c09640cf21f2276e966964
byungi <tiniwz@gmail.com>
2019-11-24 00:40:08 +0900

    refactor: cleanup

--- main.go
+++ main.go
@@ -1,156 +1,144 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
-    "os"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
 
-    human "github.com/dustin/go-humanize"
-    "github.com/skratchdot/open-golang/open"
+	human "github.com/dustin/go-humanize"
+	"github.com/skratchdot/open-golang/open"
 )
 
-type jsonRPCRequest struct {
-    Persist    bool          `json:"DontHideAfterAction"`
-    Method     string        `json:"method"`
-    Parameters []interface{} `json:"parameters"`
+type jsonRpcRequest struct {
+	Persist    bool          `json:"DontHideAfterAction"`
+	Method     string        `json:"method"`
+	Parameters []interface{} `json:"parameters"`
 }
 
-type jsonRPCResponse struct {
-    Result []jsonRPCResultItem
+type jsonRpcResponse struct {
+	Result []resultItem
 }
 
-type jsonRPCResultItem struct {
-    Title    string          `json:"Title"`
-    SubTitle string          `json:"SubTitle"`
-    IcoPath  string          `json:"IcoPath"`
-    Action   *jsonRPCRequest `json:"JsonRPCAction,omitempty"`
+type resultItem struct {
+	Title    string          `json:"Title"`
+	SubTitle string          `json:"SubTitle"`
+	IcoPath  string          `json:"IcoPath"`
+	Action   *jsonRpcRequest `json:"JsonRPCAction,omitempty"`
 }
 
 type suggestion struct {
-    Name        string `json:"name"`
-    Version     string `json:"version"`
-    Description string `json:"description"`
-    Publisher   struct {
-        Username string `json:"username"`
-        Email    string `json:"email"`
-    } `json:"publisher"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
 
 type detail struct {
-    Name        string `json:"name"`
-    Description string `json:"description"`
-    Version     string `json:"version"`
-    DepCount    uint   `json:"dependencyCount"`
-    Gzip        uint64 `json:"gzip"`
-    Size        uint64 `json:"size"`
-    Error       *struct {
-        Code    string   `json:"code"`
-        Message string   `json:"message"`
-        Details struct{} `json:"details"`
-    } `json:"error,omitempty"`
+	Gzip  uint64      `json:"gzip"`
+	Size  uint64      `json:"size"`
+	Error interface{} `json:"error,omitempty"`
 }
 
 func main() {
-    req, err := parseJsonRPCRequest(os.Args[1])
-    if err != nil {
-        return
-    }
+	req, err := parseRpcRequest(os.Args[1])
+	if err != nil {
+		return
+	}
 
-    if req.Method == "openBrowser" {
-        _ = open.Start("https://bundlephobia.com/result?p=" + req.Parameters[0].(string))
-        return
-    }
+	if req.Method == "openBrowser" {
+		_ = open.Start("https://bundlephobia.com/result?p=" + req.Parameters[0].(string))
+		return
+	}
 
-    q := strings.TrimSpace(req.Parameters[0].(string))
-    if q[len(q)-1] == '!' {
-        showDetail(strings.TrimRight(q, "!"))
-    } else {
-        suggestPkg(q)
-    }
+	q := strings.TrimSpace(req.Parameters[0].(string))
+	if q[len(q)-1] == '!' {
+		handleDetail(strings.TrimRight(q, "!"))
+	} else {
+		handlePkgSuggestion(q)
+	}
 }
 
-func parseJsonRPCRequest(str string) (*jsonRPCRequest, error) {
-    var ret *jsonRPCRequest
-    if err := json.Unmarshal([]byte(str), &ret); err != nil {
-        return nil, err
-    }
-    return ret, nil
+func parseRpcRequest(str string) (*jsonRpcRequest, error) {
+	var ret *jsonRpcRequest
+	if err := json.Unmarshal([]byte(str), &ret); err != nil {
+		return nil, err
+	}
+	return ret, nil
 }
 
-func sendQueryResult(result []jsonRPCResultItem) error {
-    b, err := json.Marshal(jsonRPCResponse{Result: result})
-    if err != nil {
-        return err
-    }
-    _, err = os.Stdout.Write(b)
-    return err
+func sendRpcResultItems(result []resultItem) error {
+	b, err := json.Marshal(jsonRpcResponse{Result: result})
+	if err != nil {
+		return err
+	}
+	_, err = os.Stdout.Write(b)
+	return err
 }
 
-func suggestPkg(q string) {
-    if len(q) < 2 {
-        return
-    }
+func handlePkgSuggestion(q string) {
+	if len(q) < 2 {
+		return
+	}
 
-    resp, err := http.Get("https://www.npmjs.com/search/suggestions?q=" + q)
-    if err != nil {
-        return
-    }
-    defer resp.Body.Close()
+	resp, err := http.Get("https://www.npmjs.com/search/suggestions?q=" + q)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
 
-    var suggestions []suggestion
-    if err = json.NewDecoder(resp.Body).Decode(&suggestions); err != nil {
-        return
-    }
+	var suggestions []suggestion
+	if err = json.NewDecoder(resp.Body).Decode(&suggestions); err != nil {
+		return
+	}
 
-    items := make([]jsonRPCResultItem, len(suggestions))
-    for idx, suggestion := range suggestions {
-        items[idx] = jsonRPCResultItem{
-            Title:    suggestion.Name,
-            SubTitle: suggestion.Description,
-            IcoPath:  "icon.png",
-            Action: &jsonRPCRequest{
-                Persist:    true,
-                Method:     "Wox.ChangeQuery",
-                Parameters: []interface{}{"nbp " + suggestion.Name + "!", true},
-            },
-        }
-    }
-    _ = sendQueryResult(items)
+	items := make([]resultItem, len(suggestions))
+	for idx, suggestion := range suggestions {
+		items[idx] = resultItem{
+			Title:    suggestion.Name,
+			SubTitle: suggestion.Description,
+			IcoPath:  "icon.png",
+			Action: &jsonRpcRequest{
+				Persist:    true,
+				Method:     "Wox.ChangeQuery",
+				Parameters: []interface{}{"nbp " + suggestion.Name + "!", true},
+			},
+		}
+	}
+	_ = sendRpcResultItems(items)
 }
 
-func showDetail(q string) {
-    resp, err := http.Get("https://bundlephobia.com/api/size?package=" + q)
-    if err != nil {
-        return
-    }
-    defer resp.Body.Close()
+func handleDetail(q string) {
+	resp, err := http.Get("https://bundlephobia.com/api/size?package=" + q)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
 
-    var detail detail
-    if err = json.NewDecoder(resp.Body).Decode(&detail); err != nil {
-        return
-    }
+	var detail detail
+	if err = json.NewDecoder(resp.Body).Decode(&detail); err != nil {
+		return
+	}
 
-    if detail.Error != nil {
-        _ = sendQueryResult([]jsonRPCResultItem{
-            {
-                Title:    "Not found : " + q,
-                SubTitle: "The package you were looking for doesn't exist.",
-                IcoPath:  "icon.png",
-            },
-        })
-        return
-    }
-    _ = sendQueryResult([]jsonRPCResultItem{
-        {
-            Title:    fmt.Sprintf("minified: %s, gzipped: %s", human.Bytes(detail.Size), human.Bytes(detail.Gzip)),
-            SubTitle: "Open your browser for more information.",
-            IcoPath:  "icon.png",
-            Action: &jsonRPCRequest{
-                Method:     "openBrowser",
-                Parameters: []interface{}{q},
-            },
-        },
-    })
+	if detail.Error != nil {
+		_ = sendRpcResultItems([]resultItem{
+			{
+				Title:    "Not found : " + q,
+				SubTitle: "The package you were looking for doesn't exist.",
+				IcoPath:  "icon.png",
+			},
+		})
+		return
+	}
+
+	_ = sendRpcResultItems([]resultItem{
+		{
+			Title:    fmt.Sprintf("minified: %s, gzipped: %s", human.Bytes(detail.Size), human.Bytes(detail.Gzip)),
+			SubTitle: "Open your browser for more information.",
+			IcoPath:  "icon.png",
+			Action: &jsonRpcRequest{
+				Method:     "openBrowser",
+				Parameters: []interface{}{q},
+			},
+		},
+	})
 }
